85ac7800037c74d6dcd198d1715fed1b896a3a2b
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-23 14:56:02 +0430

    Initial Repo

---
+++ QueueWithArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using QueueWithArray;
+
+var queue = new Queue(4);
+
+
+queue.Enqueue(5);
+queue.Enqueue(90);
+queue.Enqueue(44);
+
+queue.Dequeue();
+queue.Enqueue(44);
+
+queue.Dequeue();
+
+
+Console.WriteLine("_______________________");
+queue.Print();
---
+++ QueueWithArray/Queue.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueWithArray
+{
+    public class Queue
+    {
+        public int size;
+        public int[] queue;
+        public int front;
+        public int rear;
+        public Queue(int sizeOfQueue)
+        {
+            size = sizeOfQueue;
+            queue = new int[sizeOfQueue];
+            front = -1;
+            rear = -1;
+        }
+
+        public void Enqueue(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("The queue is full!");
+            }
+            else
+            {
+                rear++;
+                queue[rear] = item;
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+            }
+            else
+            {
+                front++;
+                int result = queue[front];
+                Console.WriteLine(result);
+
+                int j = 0;
+                for (int i = front+1; i <= rear; i++)
+                {
+                    queue[j] = queue[i];
+                    j++;
+                }
+
+                rear--;
+                front = -1;
+            }
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+            }
+            else
+            {
+                for (int i = front + 1; i <= rear; i++)
+                {
+                    Console.WriteLine(queue[i]);
+                }
+            }
+        }
+
+        public bool IsFull()
+        {
+            if (queue.Length == (rear + 1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (rear == -1 || front == rear)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
